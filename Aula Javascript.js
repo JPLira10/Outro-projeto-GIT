53841195fe663c6d73f5d1a6d83fe12e5d0c0eec
João Lira <joaopls1000@gmail.com>
2022-09-30 16:30:36 -0300

    Commit

---
+++ Aula Javascript.js
@@ -0,0 +1,32 @@
+//let numero1 ,numero2 ,divisao
+
+//numero1 = 3numero2 = 2divisao = numero1 / numero2console.log(divisao)
+
+
+/*let gasto, taxa, total
+
+prompt("Digite o"+gasto+"no supermercado")
+taxa = 0.1
+total = taxa * gasto
+console. log("O gasto foi de: "+total)
+
+let gasto, taxa, total
+gasto = parseFloat(prompt("Digite o gasto do cliente:"))
+taxa = gasto*0.1
+total = gasto+taxa
+total = total.toFixed(2)
+console.log(`O gasto total do cliente foi: R$ ${total}`) */
+
+/*let base, altura, area
+base = parseFloat(prompt("Digite a base do retângulo:"))
+altura = parseFloat(prompt("Digite a altura do retângulo:"))
+area = base * altura
+console. log("A área do retângulo é ${area}")*/
+
+
+let x, equacao
+
+
+x = parseFloat(prompt("Digite o X da equação:"))
+equacao = x**2 + 5x + 3 = 0
+console. log(`O resultado dessa equação é: ${equaçao}`)
---
+++ Exercícios.js
@@ -0,0 +1,13 @@
+var weight
+console. log(typeof weight)
+let name = Joao
+let age = 22
+let stars = 2.2
+let isSubscribed = true
+var students = [0]
+console. log (students[0])
+let studuents {
+    students
+}
+console. log (students[1])
+undefined
