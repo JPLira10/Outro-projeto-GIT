202843d0bd1ef50fed5de82aae072550e76a86b4
João Lira <joaopls1000@gmail.com>
2022-10-28 16:13:14 -0300

    varias alteracoes

--- Aula Javascript.js
+++ Aula Javascript.js
@@ -30,3 +30,130 @@ let x, equacao
 x = parseFloat(prompt("Digite o X da equação:"))
 equacao = x**2 + 5x + 3 = 0
 console. log(`O resultado dessa equação é: ${equaçao}`)
+
+
+
+
+
+/* push = adiciona um elemento ao final da lista
+
+
+
+pop = deleta ultimo elemento da lista
+
+
+
+unshift = insere um elemento no inicio da lista
+
+
+
+shift = deleta um elemento no inicio da lista
+
+
+
+toUpperCase() = Faz as letras ficarem maiusculas
+
+
+
+
+toLowerCase() = faz as letras ficarem minusculas
+
+
+
+
+replace() = troca um caractere ou palavra por outro
+
+
+
+
+typeof = retorna o tipo do caractere
+
+
+
+
+toFixed() = dtermina o numero de casas decimais
+
+
+
+While = é uma estrutura de repetiçao nao controlada
+
+
+
+
+while (ano>=0){
+
+    if(ano%400 == 0){
+        console.log(`O ano ${ano} é bissexto.`)
+    } else if ((ano%4 == 0)&&(ano%100 != 0)){
+        console.log(`O ano ${ano} é bissexto.`)
+    } else {
+        console.log(`O ano ${ano} não é bissexto.`)
+    }
+
+    ano = parseInt(prompt("Digite outro ano ou um número negativo para sair:"))
+    
+}
+
+
+
+for (let i = 2; i <= 52; i = i + 2){
+
+    console.log(i)
+
+}
+
+
+
+
+for = é uma estrutura de repetição controlada */
+
+let d=1
+let soma=0
+for (let u = 1; u <=99 ; u = u+2) {
+    soma = soma+(u/d)
+    d = d+1
+}
+console.log(soma)
+
+
+
+
+/*Faça um programa que receba do usuário uma idade qualquer e diga se se trata de uma pessoa com maior ou menor idade penal.
+Dicas: - Use a estrutura condicional com if e else;
+        - Idade deve ser passada pelo usuário.*/
+
+
+idade = parseInt(prompt("Digite quantos anos você tem:"))
+if(idade => 18) {
+    console.log("Você é um jovem")
+} else{
+    console.log("Você é um adulto")
+}
+
+
+
+
+/* Questão 2: Faça um programa que imprima a seguinte sequência: 3,6,9,...,90
+Dica:
+- Use a estrutura de repetição controlada for, lembrnado que for tem três parâmetros.
+- for(variável com o valor inicial; condição de parada; número de passos)*/
+
+
+for (let n =3; n <= 90; n = n + 3){
+    console.log(n)
+}
+
+
+
+
+/* Questão 3: Faça um programa que recebe o numerador e o denominador de uma fração, calcule a fração e retorne o valor em uma mensagem como no exemplo abaixo:
+
+Exemplo: "O resultado da fração entre 10 e 5 é: 2."
+
+Dica: Use interpolação de string com ${}*/
+
+let divisao, numerador, denominador
+numerador = parseInt(prompt("Digite o numerador da fração"))
+denominador = parseInt(prompt("Digite o denominador da fração"))
+divisao = numerador/denominador
+console.log('O resultado da fração entre '+numerador+' e '+denominador+' é: '+divisao+'.')
--- teste.js
+++ teste.js
@@ -1,7 +1,94 @@
-var v1;
-v1=console.log(prompt('Variavel 1:'))
-var v2
-v2=console.log(prompt('Variavel 1:'))
-var soma;
-soma = v1 + v2;
-console.log(Soma);
+3 : let nome, nota1, nota2, nota3, media
+nome = prompt("Digite seu nome:")
+nota1 = parseFloat(prompt("Digite a primeira nota:"))
+nota2 = parseFloat(prompt("Digite a segunda nota:"))
+nota3 = parseFloat(prompt("Digite a terceira nota:"))
+media=(nota1+nota2+nota3)/3
+if (media >= 5) {
+    console.log("Você foi aprovado")
+}
+else (media <= 5) {
+    console.log("Você foi reprovado")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let nota1
+
+nota1 = parseFloat (prompt("Digite a nota :"))
+
+if (nota1 >= 90) && (nota1 <=100) {
+    console.log("A")
+} else if ((nota1 >= 80) && (nota1 < 90)) {
+    console.log("B")
+} else if ((nota1 >= 70) && (nota1 < 80)) {
+    console.log("C")
+} else if ((nota1 >= 60) && (nota1 <70)) {
+    console.log("D")
+}
+  else if ((nota1 >= 0) && (nota1 <60)) {
+    console.log("F")
+} else {
+    console.log("Nota inválida")
+}
+
+
+
+
+
+
+
+
+
+const converteTemperatura = function (verifica, temperatura){
+
+    let booleano, C, F
+
+    if (verifica == "F") {
+        booleano = true
+    } else if (verfica == "C") {
+        booleano = false
+    } else {
+        booleano = NaN
+    } 
+
+    if (booleano == true){
+        C = temperatura
+        F = C * 9/5 + 32
+        return F
+    } else if (booleano == false) {
+        F = temperatura
+        C = (F - 32) *5/9
+        return C
+    }
+} 
+console.log(converteTemperatura("F", 33))
+
+
+
+
+idade = parseInt(prompt("Digite quantos anos você tem:"))
+if(idade >= 18) {
+    console.log("Você é um adulto")
+} else if (idade <18 ){
+    console.log("Você é um jovem")
+}
+
+
+
+
+
